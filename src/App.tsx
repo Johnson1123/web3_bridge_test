b13afebb9f69e9f23818b0f35fbcd3d20b466b96
Johnson1123 <onifadejohnson2017@gmail.com>
2024-06-14 07:58:40 -0700

    display functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Input from "./component/Input";
 import "./App.css";
+import Home from "./component/Home";
 
 const App = () => {
   return (
     <div>
       <Input />
+      <Home />
     </div>
   );
 };
